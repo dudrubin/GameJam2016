7fb453d191a3a894b561e4c00de2dda6e60995b0
davidrubin <dudi@funtomic.com>
2016-11-21 15:14:48 +0200

    Added DOTWEEN Added basic emitter container

--- Assets/EmittersControl.cs
+++ Assets/EmittersControl.cs
@@ -1,15 +1,43 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class EmittersControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	Transform leftEmitter;
+	Transform rightEmitter;
+	Transform timerBAr;
+	private Object enemyBase;
+
+	void Awake() {
+		leftEmitter = transform.FindChild("EmitterLeft");
+		rightEmitter = transform.FindChild("EmitterRight");
+		timerBAr = transform.FindChild("TimerContainer");
+		enemyBase = Resources.Load("Prefabs/Enemy");
+		NewWave();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	///
+	/// </summary>
+	public void NewWave() {
+		float waveDuration = 10;
+		Sequence sequence = DOTween.Sequence();
+		sequence.Append(timerBAr.DOScaleX(1, 0.05f));
+		sequence.AppendCallback(Emit);
+		sequence.Append(timerBAr.DOScaleX(0, waveDuration));
+		sequence.AppendCallback(NewWave);
+
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	private void Emit() {
+		GameObject left = Instantiate(enemyBase) as GameObject;
+		GameObject right = Instantiate(enemyBase) as GameObject;
+		left.transform.SetParent(transform);
+		right.transform.SetParent(transform);
+		left.transform.localPosition = leftEmitter.localPosition;
+		right.transform.localPosition = rightEmitter.localPosition;
 	}
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Main : MonoBehaviour {
 
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		//GameObject.Instantiate(Resources.Load("Prefabs/Temp"));
 	}
 	
