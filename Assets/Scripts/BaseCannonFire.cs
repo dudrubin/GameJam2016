bcd76207a6a6adc71c659dfe85af850bc3c1e327
Sahar <sahar@funtomic.com>
2016-11-22 11:55:03 +0200

    basic cannon movement/fire handlers

---
+++ Assets/Scripts/BaseCannonFire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BaseCannonFire
+{
+	Transform[] barrelTransforms;
+	float projectileSpeed = 10;
+	List<GameObject> cannonProjectiles;
+	public BaseCannonFire (Transform[] barrelTransforms)
+	{
+		this.barrelTransforms = barrelTransforms;
+		this.cannonProjectiles = new List<GameObject> (); 
+	}
+
+	public virtual void RespondToInput(Vector2[] touchPoints){
+		if (touchPoints.Length > 0) {
+			Vector3 angle = barrelTransforms[0].localEulerAngles;
+			GameObject ProjectileObj = GameObject.Instantiate (Resources.Load("Prefabs/Projectile")) as GameObject;
+			Projectile projectile = ProjectileObj.GetComponent<Projectile> ();
+			ProjectileObj.transform.position = barrelTransforms[0].position;
+			ProjectileObj.transform.localEulerAngles = angle;
+			Vector2 velocity = new Vector2 (-projectileSpeed * Mathf.Sin (angle.z * Mathf.Deg2Rad), projectileSpeed * Mathf.Cos (angle.z * Mathf.Deg2Rad));
+			projectile.SetVelocity (velocity);
+			cannonProjectiles.Add (ProjectileObj);
+		}
+	}
+}
+
+
---
+++ Assets/Scripts/BaseCannonMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+
+public class BaseCannonMovement 
+{
+	Transform[] barrelTransforms;
+
+	public BaseCannonMovement ( Transform[] barrelTransforms )
+	{
+		this.barrelTransforms = barrelTransforms;
+	}
+
+	public void RespondToInput(Vector2[] touchPoints){
+		if (touchPoints.Length > 0) {
+			float deltaX = barrelTransforms[0].position.x - touchPoints[0].x;
+			float deltaY = barrelTransforms[0].position.y - touchPoints[0].y;
+			float barrelAngle = -(float)(Mathf.Atan (deltaX / deltaY) * 180.0 / 3.14);
+			barrelTransforms[0].transform.localEulerAngles = new Vector3 (){ x = 0, y = 0, z = barrelAngle };
+		}
+	}
+}
+
+
