b9e7989e4ff8ad3cab7c4e7e7b1775ec4d78e6d2
Sahar <sahar@funtomic.com>
2016-11-22 11:55:25 +0200

    basic cannon movement/fire handlers

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -4,17 +4,16 @@ using System.Collections.Generic;
 public class Cannon : MonoBehaviour {
 
 	GameObject barrelRef;
-	Main gameRef;
-	List<GameObject> cannonProjectiles;
+
 	BaseCannonMovement movementHandler;
-	float PROJECTILE_SPEED = 10;
+	BaseCannonFire fireHandler;
+
 
 	// Use this for initialization
 	void Start () {
 		barrelRef = GameObject.Find ("Barrel");
-		gameRef = GetComponentInParent<Main> ();
-		cannonProjectiles = new List<GameObject> ();
 		movementHandler = new BaseCannonMovement (new Transform[] { barrelRef.transform});
+		fireHandler = new BaseCannonFire (new Transform[] { barrelRef.transform });
 	}
 
 
@@ -24,20 +23,10 @@ public class Cannon : MonoBehaviour {
 		if (!getTouchPos(out pos))
 			return;
 		movementHandler.RespondToInput (new Vector2[]{ pos });
-		createProjectile (barrelRef.transform.position, barrelRef.transform.localEulerAngles);
+		fireHandler.RespondToInput (new Vector2[]{ pos });
 	}
 
-	private void createProjectile(Vector2 pos, Vector3 angle){
-		GameObject ProjectileObj = GameObject.Instantiate (Resources.Load("Prefabs/Projectile")) as GameObject;
-		Projectile projectile = ProjectileObj.GetComponent<Projectile> ();
-		ProjectileObj.transform.position = pos;
-		ProjectileObj.transform.localEulerAngles = angle;
-		Vector2 velocity = new Vector2 (-PROJECTILE_SPEED * Mathf.Sin (angle.z * Mathf.Deg2Rad), PROJECTILE_SPEED * Mathf.Cos (angle.z * Mathf.Deg2Rad));
-		Debug.LogFormat ("New projectile: pos:{0} angle {1} velocity: {2}", pos, angle.z, velocity);
-		Debug.LogFormat ("cos angle x: {0} sin angle : {1}", Mathf.Cos (angle.z * Mathf.Deg2Rad), Mathf.Sin (angle.z * Mathf.Deg2Rad) );
-		projectile.SetVelocity (velocity);
-		cannonProjectiles.Add (ProjectileObj);
-	}
+
 
 	private bool getTouchPos(out Vector2 touchPos){
 		touchPos = new Vector2();
